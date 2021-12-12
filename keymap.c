3fa6a58e0f464a3ea2504e652b20b6970b1b0fdd
tyesc <escolano.tyler@gmail.com>
2021-12-12 14:58:11 +0100

    udpate

--- keymap.c
+++ keymap.c
@@ -24,11 +24,13 @@ enum preonic_layers {
   MO_2,
   FN,
   _MAC_KEYBOARD,
+  // _MIDI,
 };
 
 enum preonic_keycodes {
   WIN_LINUX = SAFE_RANGE,
   MAC_KEYBOARD,
+  // MIDI
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -114,10 +116,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, KC_NO, KC_NO, KC_MPRV, KC_MPLY, KC_MNXT, KC_NO,
    KC_NO, RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, KC_NO, KC_NO, KC_NO, KC_VOLU, KC_MUTE, KC_NO,
    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_BRID,  KC_BRIU,  KC_VOLD,  KC_NO,  KC_NO,
-   KC_NO,  KC_TRNS, KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  WIN_LINUX,  MAC_KEYBOARD
+   KC_NO,  KC_TRNS, KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  MIDI,  KC_NO,  WIN_LINUX,  MAC_KEYBOARD
 ),
 
 /* _MAC_KEYBOARD
+ * ,-----------------------------------------------------------------------------------.
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Esc  |   A  |   Z  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  '   |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Tab  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   M  |Enter |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   ,  |   .  |   /  |   ;  |Shift |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Ctrl |MO(Fn)|  Os  | Alt  | MO(1)|    Space    |Raise | MO(2) |  Up  |Down |Right |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_MAC_KEYBOARD] = LAYOUT_preonic_grid(
+  KC_GRV,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0,  KC_BSPC,
+  KC_ESC,  KC_A,  KC_Z,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_QUOT,
+  KC_TAB,  KC_Q,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  KC_M,  KC_ENT,
+  KC_LSFT, KC_W,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N, KC_COMM, KC_DOT, KC_SLSH, KC_SCLN, KC_RSFT,
+  KC_LCTL, MO(FN), KC_LGUI, KC_LALT, MO(MO_1), KC_SPC, MO(MO_2), KC_LEFT, KC_UP, KC_DOWN, KC_RGHT
+),
+
+/* _MIDI
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -130,34 +153,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Ctrl |MO(Fn)|  Os  | Alt  | MO(1)|    Space    |Raise | MO(2) |  Up  | Down |Right |
  * `-----------------------------------------------------------------------------------'
  */
-[_MAC_KEYBOARD] = LAYOUT_preonic_grid(
-  KC_GRV,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0,  KC_BSPC,
-  KC_ESC,  KC_A,  KC_Z,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_QUOT,
-  KC_TAB,  KC_Q,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  KC_M,  KC_ENT,
-  KC_LSFT, KC_W,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N, KC_COMM, KC_DOT, KC_SLSH, KC_SCLN, KC_RSFT,
-  KC_LCTL, MO(FN), KC_LGUI, KC_LALT, MO(MO_1), KC_SPC, MO(MO_2), KC_LEFT, KC_UP, KC_DOWN, KC_RGHT
-),
+// [_MIDI] = LAYOUT_preonic_grid(
+//   KC_GRV,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0,  KC_BSPC,
+//   KC_ESC,  KC_A,  KC_Z,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_QUOT,
+//   KC_TAB,  KC_Q,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  KC_M,  KC_ENT,
+//   KC_LSFT, KC_W,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N, KC_COMM, KC_DOT, KC_SLSH, KC_SCLN, KC_RSFT,
+//   KC_LCTL, MO(FN), KC_LGUI, KC_LALT, KC_NO, KC_SPC, KC_NO, KC_LEFT, KC_UP, KC_DOWN, KC_RGHT
+// ),
 
-/* Adjust (Lower + Raise)
- * ,-----------------------------------------------------------------------------------.
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Reset| Debug|      |      |      |      |TermOf|TermOn|      |      |  Del |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |Aud cy|Aud on|AudOff|AGnorm|AGswap|Qwerty|Colemk|Dvorak|      |      |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |Voice-|Voice+|Mus on|MusOff|MidiOn|MidOff|      |      |      |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      |      |      |      |
- * `-----------------------------------------------------------------------------------'
- */
-// [_ADJUST] = LAYOUT_preonic_grid(
-//   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-//   _______, RESET,   DEBUG,   _______, _______, _______, _______, TERM_ON, TERM_OFF,_______, _______, KC_DEL,
-//   _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, DVORAK,  _______, _______,
-//   _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______,
-//   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
-// )
+
 
 
 };
@@ -176,6 +180,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           }
           return false;
           break;
+        // case MIDI:
+        //   if (record->event.pressed) {
+        //     set_single_persistent_default_layer(_MIDI);
+        //   }
+        //   return false;
+        //   break;
         // case LOWER:
         //   if (record->event.pressed) {
         //     layer_on(FN);
